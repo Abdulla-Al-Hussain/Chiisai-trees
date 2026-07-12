9299193a28b6d230d7380a227190cbdca4d307e4
Abdulla-Al-Hussain <155281916+Abdulla-Al-Hussain@users.noreply.github.com>
2026-07-12 11:33:04 +0600

    Fix import case for MainLayout in Router.jsx

--- src/routes/Router.jsx
+++ src/routes/Router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import MainLayout from "../layouts/MainLayout";
+import MainLayout from "../layouts/Mainlayout";
 import Home from "../pages/Home/Home";
 import Plants from "../pages/Plants/Plants";
 import PlantDetails from "../pages/PlantDetails/PlantDetails";
